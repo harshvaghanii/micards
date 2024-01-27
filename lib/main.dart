f3712ea7076ce5d69abd0c8c312284ed7c1a0e2d
Harsh Vaghani <harshvaghani98@gmail.com>
2024-01-26 20:35:51 -0500

    Starting with Containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+/**
+ * For the Hot reload to work, the components should be in the Stateless or stateful widget
+ * Hence, the Material App is returned inside of the MyApp class which is a Stateless widget.
+ */
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  /**
+   * Important Widgets:
+   * SafeArea: The widgets inside SafeArea will be placed outside the bezels and the notch of the phone
+   * 
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text("This is a container!"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
