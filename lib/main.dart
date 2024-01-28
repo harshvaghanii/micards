4107bbc27c80c0f9277e72b072bb668a44bb1713
Harsh Vaghani <harshvaghani98@gmail.com>
2024-01-28 12:06:45 -0500

    Completing the MI Cards application

--- lib/main.dart
+++ lib/main.dart
@@ -13,21 +13,145 @@ class MyApp extends StatelessWidget {
   /**
    * Important Widgets:
    * SafeArea: The widgets inside SafeArea will be placed outside the bezels and the notch of the phone
-   * 
+   *
    */
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            child: Text("This is a container!"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/harshVaghani_Profile.jpg"),
+              ),
+              Text(
+                "Harsh Vaghani",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "SOFTWARE ENGINEER",
+                style: TextStyle(
+                  fontFamily: 'SourseSans3',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(1.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+1 (607) 296 8094",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: "SourceSans3",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(1.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "hvaghani@binghamton.edu",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: "SourceSans3",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+// App Challenge
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.teal,
+//         body: SafeArea(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Container(
+//                 color: Colors.red,
+//                 width: 100.0,
+//               ),
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.yellow,
+//                     width: 100.0,
+//                     height: 100.0,
+//                   ),
+//                   Container(
+//                     color: Colors.green,
+//                     width: 100.0,
+//                     height: 100.0,
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 color: Colors.blue,
+//                 width: 100.0,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
